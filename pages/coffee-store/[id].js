120b0e4c27edf4f4be0f38a9df423466c018115a
Filipe Valente Gomes <Filipe@imac.myfiosgateway.com>
2022-04-13 02:23:11 -0400

    added getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,17 +1,57 @@
 import Link from "next/link";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import coffeeStoresData from "../../Data/coffee-stores.json";
+import Head from "next/head";
 
-function CoffeeStore() {
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(coffeeStore => {
+        return coffeeStore.id.toString() === params.id;
+    }),
+    },
+  };
+}
+
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) =>{
+    return{
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+
+  return{
+    paths,
+    fallback: true
+  }
+}
+
+function CoffeeStore({coffeeStore}) {
   const router = useRouter();
   console.log(router)
+
+
+  if(router.isFallback){
+    return(
+      <div>Loading ..</div>
+    )
+  }
+
+  const {address, name, neighbourhood } = coffeeStore;
+
   return (
-    <div>Coffee Store Page {router.query.id}
+    <div>
+      <Head><title>{name}</title></Head>
       <Link href="/">
         <a>Back to home</a>
       </Link>
-      <Link href="/coffee-store/dynamic">
-        <a>Go to page dynamic</a>
-      </Link>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   )
 }
