257c601f6c033930ec84da797936ab05259d7b8e
Filipe Valente Gomes <Filipe@imac.myfiosgateway.com>
2022-04-12 23:45:32 -0400

    added getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,20 @@ import Image from "next/image"
 import styles from "../styles/Home.module.scss"
 import Banner from "../components/banner"
 import Card from "../components/card"
-import coffeeStores from "../data/coffee-stores.json"
+import coffeeStoresData from "../data/coffee-stores.json"
 
-export default function Home() {
+export async function getStaticProps(context) {
+  console.log("Hi getStaticProps");
+  return {
+    props: {
+      coffeeStores: coffeeStoresData,
+    }, 
+  }
+}
+
+export default function Home({coffeeStores}) {
+
+  console.log(coffeeStores);
 
   const handleOnBannerButtonClick = () => {
     console.log("Hi, banner button")
@@ -30,20 +41,23 @@ export default function Home() {
           height={400}
         />
         </div>
-        <div className={styles.cardLayout}>
-        {coffeeStores.map((coffeeStore) => {
-          const {name, imgUrl, id} = coffeeStore;
-          return(
-            <Card 
-              key={id}
-              name={name} 
-              imgUrl={imgUrl} 
-              href={`/coffee-store/${id}`} 
-            />
-          )
-        })}
-        
-        </div>
+        {coffeeStores.length > 0 && 
+        <>
+          <h2 className={styles.heading2}>Toronto stores</h2>
+          <div className={styles.cardLayout}>
+          {coffeeStores.map((coffeeStore) => {
+            const {name, imgUrl, id} = coffeeStore;
+            return(
+              <Card 
+                key={id}
+                name={name} 
+                imgUrl={imgUrl} 
+                href={`/coffee-store/${id}`} 
+              />
+            )
+          })}
+          </div>
+        </>}
       </main>
 
     </div>
